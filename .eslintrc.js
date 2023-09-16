dcc58b9f059d8b624ad83e449b6cec5454ec7e4c
Nnamani Ugochukwu Frederick <royal.ugoh@gmail.com>
2023-09-16 12:48:30 +0100

    Implement Put, Patch And Delete Http Verbs

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,5 +22,6 @@ module.exports = {
     ecmaVersion: 'latest',
   },
   rules: {
+    'undescore-dangle': 0,
   },
 };
--- routes/bookRouter.js
+++ routes/bookRouter.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-underscore-dangle */
 const { Router } = require('express');
 const debug = require('debug')('app:bookRouter');
 
@@ -27,15 +28,62 @@ function router(Book) {
       }
     });
 
-  bookRoutes.route('/books/:bookId').get(async (req, res) => {
-    const { bookId } = req.params;
-    try {
-      const book = await Book.findById(bookId);
-      return res.status(200).json(book);
-    } catch (err) {
-      return debug(err);
-    }
-  });
+  bookRoutes.route('/books/:bookId')
+    .all(async (req, res, next) => {
+      const { bookId } = req.params;
+      try {
+        const book = await Book.findById(bookId);
+        if (!book) {
+          return res.status(404).json({ message: 'Book Not Found.' });
+        }
+        req.book = book;
+        return next();
+      } catch (err) {
+        debug(err);
+        return res.status(500);
+      }
+    })
+    .get(async (req, res) => res.status(200).json(req.book))
+    .put(async (req, res) => {
+      const { book } = req;
+      if (req.body._id) {
+        delete req.body._id;
+      }
+      book.title = req.body.title;
+      book.author = req.body.author;
+      book.read = req.body.read;
+      book.genre = req.body.genre;
+      try {
+        await book.save();
+        return res.status(201).json(book);
+      } catch (err) {
+        debug(err);
+        return res.status(500).json(err);
+      }
+    })
+    .patch(async (req, res) => {
+      const { book } = req;
+      try {
+        Object.entries(req.body).forEach((keyValue) => {
+          const [key, value] = keyValue;
+          book[key] = value;
+        });
+        await book.save();
+        return res.status(201).json(book);
+      } catch (err) {
+        debug(err);
+        return res.status(500).json(err);
+      }
+    })
+    .delete(async (req, res) => {
+      try {
+        await Book.deleteOne({ _id: req.params.bookId });
+        return res.status(204).json({ message: 'Book Successfullyy Deleted' });
+      } catch (err) {
+        debug(err);
+        return res.status(500).json(err);
+      }
+    });
 
   return bookRoutes;
 }
